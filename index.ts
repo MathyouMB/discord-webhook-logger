fb1dea020b4f95857ba1447dd3c3818c3770c0a6
Matthew M-B <matthew.mb@getjobber.com>
2024-12-16 00:20:34 -0500

    Add version bump 0.1.3

--- index.ts
+++ index.ts
@@ -1,9 +1,1 @@
-import { DiscordWebhookLogger } from "./lib";
-
-const logger = new DiscordWebhookLogger({
-  webhookUrls: [
-    "https://discord.com/api/webhooks/1316625557777023027/88zfwzSV7RzsN2W_Fb1J-XeDgNz-jZ7CAu9MuuDhRWvz5BbZ6cKHEEWjaivJBe6NAMjP",
-  ],
-});
-
-logger.error("This is an error message");
+export * from "./lib";
